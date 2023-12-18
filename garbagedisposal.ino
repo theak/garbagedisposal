fe5c1c3c17e0fec6ca573a54995f08ee752ab962
Akshay Kannan <theak@users.noreply.github.com>
2023-12-17 22:09:14 -0800

    Add files via upload

---
+++ garbagedisposal.ino
@@ -0,0 +1,43 @@
+//GLOBAL VARS
+bool inverted = false;
+bool pressed = false;
+bool activated = false;
+unsigned long pressedTime = 0;
+unsigned long activationTime = 0;
+
+//CONSTANTS
+const unsigned long duration = 7000;
+const unsigned long minReleaseDelay = 100;
+const unsigned long minInvertDelay = 10000;
+
+void setup() {
+  pinMode(0, INPUT_PULLUP); //Push button (HIGH = off, LOW = on)
+  pinMode(12, OUTPUT); //Relay and its status LED
+  pinMode(13, OUTPUT); //Green LED (HIGH = off, LOW = on)
+
+}
+
+void loop() {
+  if (digitalRead(0) == inverted) {
+    pressed = true;
+    pressedTime = millis();
+    digitalWrite(13, HIGH);
+  }
+  else {
+    if (pressed) {
+      if ((millis()  - pressedTime) > minInvertDelay) inverted = !inverted;
+      else if ((millis()  - pressedTime) > minReleaseDelay) {
+        digitalWrite(12, HIGH);
+        activated = true;
+        activationTime = millis();
+      }
+    }
+    pressed = false;
+    digitalWrite(13, LOW);
+  }
+
+  if (activated && ((millis() - activationTime) > duration)) {
+    digitalWrite(12, LOW);
+    activated = false;
+  }
+}
